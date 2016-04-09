375fde75d667fdc95912d29011a9566516e3c857
Stephen Sullivan <sullivankstephen@gmail.com>
2016-04-09 13:40:52 -0500

    Basic untested script & sample.

---
+++ scripts/process.py
@@ -0,0 +1,28 @@
+from scipy.io.wavfile import read
+from scipy import signal
+from scipy.fftpack import fft
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+
+# Read file to get samplerate and numpy array containing the signal 
+(fs, x) = read("../someaudio.wav")
+
+
+channels = [
+    np.array(x[:, 0]),
+    np.array(x[:, 1])
+]
+
+# Combine channels to make a mono signal out of stereo
+channel =  np.mean(channels, axis=0)
+
+# Generate spectrogram 
+## Freqs is the same with different songs, t differs slightly
+Pxx, freqs, t, plot = pylab.specgram(
+    channel,
+    NFFT=2048, 
+    Fs=44100, 
+    detrend=pylab.detrend_none,
+    window=pylab.window_hanning,
+    noverlap=int(2048 * 0.5))
