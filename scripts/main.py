2ef8b74417e517ee290fcda74dd9835fcf9a25fa
Mazin Bokhari <mbokhar2@illinois.edu>
2016-04-12 04:46:55 -0500

    main.py now handles directories :::)))

--- scripts/main.py
+++ scripts/main.py
@@ -84,90 +84,6 @@ def generate_spectrograms(source_file):
 
 
 
-
-# def compute_distance(source_file,target_file,sampling_window_index):
-# 	specs_file = open('../temp/{}.specs.npz'.format(source_file), 'r')
-# 	spectrograms = np.load(specs_file)
-
-# 	target_specs_file = open('../temp/{}.specs.npz'.format(target_file), 'r')
-# 	target_spectrograms = np.load(target_specs_file)
-
-
-
-# 	# pca shit
-# 	n_samples = target_spectrograms.shape[0]
-# 	h = target_spectrograms.shape[1]
-# 	w = target_spectrograms.shape[2]
-
-# 	data = target_spectrograms.reshape((n_samples,h*w))
-# 	n_components = 100
-
-# 	# print("Extracting the top %d eigensounds from %d windows..." % (n_components, n_samples))
-# 	# t0 = time()
-# 	# pca = RandomizedPCA(n_components=n_components, whiten=True).fit(data)
-# 	# print("Done in %0.3fs." % (time() - t0))
-
-# 	# figure = plt.figure()
-# 	# plt.plot(map(lambda v:  norm(v), pca.components_))
-# 	# figure.savefig('../pca.png')
-# 	# plt.close()
-
-# 	# eigensounds = pca.components_.reshape((n_components, h, w))
-
-# 	# figure = plt.figure()
-# 	# plt.imshow(eigensounds[0])
-# 	# figure.savefig('./eigen.png')
-
-# 	# print("Drawing eigensound spectrograms...")
-# 	# for i in range(n_components):
-# 	# 	figure = plt.figure()
-# 	# 	# plt.imshow(spectrograms[0])
-# 	# 	# figure.savefig('./spec.png')
-# 	# 	plt.pcolormesh(bins, freqs, 10 * np.log10(abs(eigensounds[i])))
-# 	# 	plt.axis('tight')
-# 	# 	figure.savefig('./eigen-{}.png'.format(i))
-# 	# 	plt.close()
-
-# 	# print("Projecting the input data on the eigensounds...")
-# 	# t0 = time()
-# 	# pca_projected_data = pca.transform(data)
-# 	# print("Done in %0.3fs." % (time() - t0))
-
-# 	n_clusters = 60
-
-
-# 	# cluster the source file
-# 	print("Computing clustering for each PCA projected window...")
-# 	print(spectrograms.shape)
-# 	# sys.exit()
-# 	t0 = time()
-# 	source_clusterings = map(lambda sample: MiniBatchKMeans(n_clusters=n_clusters).fit(sample).cluster_centers_, spectrograms)
-# 	print("Done in %0.3fs." % (time() - t0))
-
-
-# 	# cluster the target file
-# 	print("Computing clustering for each PCA projected window...")
-# 	print(target_spectrograms.shape)
-# 	# sys.exit()
-# 	t0 = time()
-# 	target_clusterings = map(lambda sample: MiniBatchKMeans(n_clusters=n_clusters).fit(sample).cluster_centers_, target_spectrograms)
-# 	print("Done in %0.3fs." % (time() - t0))
-
-
-# 	print("Computing closest window to specified target window...")
-# 	t0 = time()
-# 	sample_window_clustering = source_clusterings[sampling_window_index]
-# 	distances_to_sample_window_clustering = map(lambda curr_target_window_clustering: norm(curr_target_window_clustering - sample_window_clustering), target_clusterings)
-# 	print("Done in %0.3fs." % (time() - t0))
-
-# 	# indexed_distances = np.stack((distances_to_target_clustering,np.arange(len(distances_to_target_clustering))),1)
-# 	figure = plt.figure()
-# 	plt.plot(distances_to_sample_window_clustering)
-# 	figure.savefig('../results/distances_from_{}_at_{}_to_{}.png'.format(source_file,sampling_window_index,target_file))
-# 	# plt.show()
-# 	plt.close()
-
-
 def compute_distance(source_file,target_file,sampling_window_index):
 	specs_file = open('../temp/{}.specs.npz'.format(source_file), 'r')
 	spectrograms = np.load(specs_file)
@@ -242,6 +158,11 @@ def compute_distance(source_file,target_file,sampling_window_index):
 	# indexed_distances = np.stack((distances_to_target_clustering,np.arange(len(distances_to_target_clustering))),1)
 	figure = plt.figure()
 	plt.plot(distances_to_target_clusterings)
+
+
+	# get rid of slashes and shit
+	target_file = target_file.split("/")[-1]
+
 	figure.savefig('../results/distances_from_{}_at_{}_to_{}.png'.format(source_file,sampling_window_index,target_file))
 	# plt.show()
 	plt.close()
@@ -273,9 +194,18 @@ def main():
 
 	if target_type == '-f':
 		generate_spectrograms(target_name)
-		print('compute_distance({},{},{})'.format(source_file,target_name,sampling_window_index))
+		# print('compute_distance({},{},{})'.format(source_file,target_name,sampling_window_index))
 		compute_distance(source_file,target_name,sampling_window_index)
 
+	elif target_type == '-d':
+		for target_file in os.listdir('../samples/{}'.format(target_name)):
+			if not os.path.exists("../temp/{}/".format(target_name)):
+			    os.makedirs("../temp/{}/".format(target_name))
+
+			# print "{}/{}".format(target_name,target_file)
+			generate_spectrograms("{}/{}".format(target_name,target_file))
+			# print('compute_distance({},{},{})'.format(source_file,"{}/{}".format(target_name,target_file),sampling_window_index))
+			compute_distance(source_file,"{}/{}".format(target_name,target_file),sampling_window_index)
 
 
 
