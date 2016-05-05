f12e669358fa83828c1dff795ddb8014fcfcc06d
Mazin Bokhari <mbokhar2@illinois.edu>
2016-05-04 21:04:36 -0500

    Refactored more

--- scripts/mfcc_analyze.py
+++ scripts/mfcc_analyze.py
@@ -10,11 +10,12 @@ import sys
 from time import time
 import warnings
 
-# actually, we're not computing the cross-dot=products, just the diagonals (w sums)... so maybe we can try doing that 
-# Should take an audio file, compute the mfcc (which is basically a feature vector), then computes the similarity matrix with square dimensions (dim(mfcc)/w)-squared by averaging over every cross=dot=product for a window of size w
-# thus, there are w*w sums
+# actually, we're not computing the cross-dot=products, just the diagonals (sampling_window_length sums)... so maybe we can try doing that 
+# Should take an audio file, compute the mfcc (which is basically a feature vector), then computes the similarity matrix with square dimensions (dim(mfcc)/sampling_window_length)-squared by averaging over every cross=dot=product for a window of size sampling_window_length
+# thus, there are sampling_window_length*sampling_window_length sums
 
 w = 10
+sampling_window_length = 10
 audio_file = sys.argv[1]
 
 warnings.filterwarnings("ignore", category=wav.WavFileWarning)
@@ -32,16 +33,16 @@ def compute_similarity_matrix(mfcc_feat):
         print("Computing similarity matrix...")
         t0 = time()
         sig_length = len(mfcc_feat)
-        similarity_matrix = np.ndarray((sig_length/w,sig_length/w))
-        for i in range(sig_length/w):
-                for j in range(sig_length/w):
+        similarity_matrix = np.ndarray((sig_length/sampling_window_length,sig_length/sampling_window_length))
+        for i in range(sig_length/sampling_window_length):
+                for j in range(sig_length/sampling_window_length):
                         entry = 0
-                        for k in range(w):
-                                entry += np.dot(mfcc_feat[i*w+k,:],mfcc_feat[j*w+k,:])/(la.norm(mfcc_feat[i*w+k,:])*la.norm(mfcc_feat[j*w+k,:]))
-                                #entry += np.dot(mfcc_feat[i*w+k,:],mfcc_feat[j*w+k,:])
-                        similarity_matrix[i:i+w,j:j+w] = entry/float(w) 
-                        print "{0:2.0f}%\b\b\b\b".format(100*float(i*w*sig_length+j*w)/float(sig_length*sig_length)),
-                        # print(i*wlen(mfcc_feat)+j*w)
+                        for k in range(sampling_window_length):
+                                entry += np.dot(mfcc_feat[i*sampling_window_length+k,:],mfcc_feat[j*sampling_window_length+k,:])/(la.norm(mfcc_feat[i*sampling_window_length+k,:])*la.norm(mfcc_feat[j*sampling_window_length+k,:]))
+                                #entry += np.dot(mfcc_feat[i*sampling_window_length+k,:],mfcc_feat[j*sampling_window_length+k,:])
+                        similarity_matrix[i:i+sampling_window_length,j:j+sampling_window_length] = entry/float(sampling_window_length) 
+                        print "{0:2.0f}%\b\b\b\b".format(100*float(i*sampling_window_length*sig_length+j*sampling_window_length)/float(sig_length*sig_length)),
+                        # print(i*wlen(mfcc_feat)+j*sampling_window_length)
                         # print(float(len(mfcc_feat)*len(mfcc_feat)))
                         sys.stdout.flush()
         print("Done in %0.3fs." % (time() - t0))
@@ -64,10 +65,14 @@ def save_matrix(input_matrix):
     plt.title(audio_file)
     plt.imshow(input_matrix, cmap="gray",interpolation='none',origin='lower')
     #plt.show()
-    figure.savefig(audio_file[:-4]+"_w_"+str(w)+".png")
+    figure.savefig(audio_file[:-4]+"_sampling_window_length_"+str(sampling_window_length)+".png")
     print("Done in %0.3fs." % (time() - t0))
 
-mfcc_feat = compute_mfcc()
-similarity_matrix = compute_similarity_matrix(mfcc_feat)
-normalized_matrix = normalize_matrix(similarity_matrix)
-save_matrix(normalized_matrix)
+def main():
+    mfcc_feat = compute_mfcc()
+    similarity_matrix = compute_similarity_matrix(mfcc_feat)
+    normalized_matrix = normalize_matrix(similarity_matrix)
+    save_matrix(normalized_matrix)
+    
+if __name__ == "__main__":
+    main()
