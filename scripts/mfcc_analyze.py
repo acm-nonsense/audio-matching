80d731b0c25a7a0f383f2d4a83a1ea683977016b
Mazin Bokhari <mbokhar2@illinois.edu>
2016-05-04 22:59:39 -0500

    Add functionality

--- scripts/mfcc_analyze.py
+++ scripts/mfcc_analyze.py
@@ -17,6 +17,7 @@ import warnings
 #w = 10
 sampling_window_length = 10
 audio_file = sys.argv[1]
+file_head_length = 10 # in seconds
 
 warnings.filterwarnings("ignore", category=wav.WavFileWarning)
 
@@ -24,29 +25,30 @@ def compute_mfcc():
     print("\tComputing MFCCs...")
     t0 = time()
     (rate,sig) = wav.read(audio_file)
+    sig = sig[0:rate*file_head_length,:] # delete this after testing
     mfcc_feat = mfcc(sig,rate)
     #fbank_feat = logfbank(sig,rate) # potentially look at this later?
     print("\tDone in %0.3fs." % (time() - t0))
     return mfcc_feat
 
 def compute_similarity_matrix(mfcc_feat):
-        print("\tComputing similarity matrix...")
-        t0 = time()
-        sig_length = len(mfcc_feat)
-        similarity_matrix = np.ndarray((sig_length/sampling_window_length,sig_length/sampling_window_length))
-        for i in range(sig_length/sampling_window_length):
-                for j in range(sig_length/sampling_window_length):
-                        entry = 0
-                        for k in range(sampling_window_length):
-                                entry += np.dot(mfcc_feat[i*sampling_window_length+k,:],mfcc_feat[j*sampling_window_length+k,:])/(la.norm(mfcc_feat[i*sampling_window_length+k,:])*la.norm(mfcc_feat[j*sampling_window_length+k,:]))
-                                #entry += np.dot(mfcc_feat[i*sampling_window_length+k,:],mfcc_feat[j*sampling_window_length+k,:])
-                        similarity_matrix[i:i+sampling_window_length,j:j+sampling_window_length] = entry/float(sampling_window_length) 
-                        print "\t{0:2.0f}%\b\b\b\b\b".format(100*float(i*sampling_window_length*sig_length+j*sampling_window_length)/float(sig_length*sig_length)),
-                        # print(i*wlen(mfcc_feat)+j*sampling_window_length)
-                        # print(float(len(mfcc_feat)*len(mfcc_feat)))
-                        sys.stdout.flush()
-        print("\tDone in %0.3fs." % (time() - t0))
-        return similarity_matrix
+    print("\tComputing similarity matrix...")
+    t0 = time()
+    sig_length = len(mfcc_feat)
+    similarity_matrix = np.ndarray((sig_length/sampling_window_length,sig_length/sampling_window_length))
+    for i in range(sig_length/sampling_window_length):
+            for j in range(sig_length/sampling_window_length):
+                    entry = 0
+                    for k in range(sampling_window_length):
+                            entry += np.dot(mfcc_feat[i*sampling_window_length+k,:],mfcc_feat[j*sampling_window_length+k,:])/(la.norm(mfcc_feat[i*sampling_window_length+k,:])*la.norm(mfcc_feat[j*sampling_window_length+k,:]))
+                            #entry += np.dot(mfcc_feat[i*sampling_window_length+k,:],mfcc_feat[j*sampling_window_length+k,:])
+                    similarity_matrix[i:i+sampling_window_length,j:j+sampling_window_length] = entry/float(sampling_window_length) 
+                    print "\t{0:2.0f}%\b\b\b\b\b".format(100*float(i*sampling_window_length*sig_length+j*sampling_window_length)/float(sig_length*sig_length)),
+                    # print(i*wlen(mfcc_feat)+j*sampling_window_length)
+                    # print(float(len(mfcc_feat)*len(mfcc_feat)))
+                    sys.stdout.flush()
+    print("\tDone in %0.3fs." % (time() - t0))
+    return similarity_matrix
                     
 def standardize_matrix(input_matrix):
     print("\tStandardizing similarity matrix...")
@@ -58,14 +60,14 @@ def standardize_matrix(input_matrix):
     return input_matrix
 
 def normalize_matrix(input_matrix):
-        print("\tNormalizing similarity matrix...")
-        t0 = time()
-        max = input_matrix.max()
-        min = input_matrix.min()
-        input_matrix -= min
-        input_matrix /= (max-min)
-        print("\tDone in %0.3fs." % (time() - t0))
-        return input_matrix
+    print("\tNormalizing similarity matrix...")
+    t0 = time()
+    max = input_matrix.max()
+    min = input_matrix.min()
+    input_matrix -= min
+    input_matrix /= (max-min)
+    print("\tDone in %0.3fs." % (time() - t0))
+    return input_matrix
 
 def save_matrix(input_matrix):
     print("\tVisualizing similarity matrix...")
@@ -74,7 +76,8 @@ def save_matrix(input_matrix):
     plt.title(audio_file)
     plt.imshow(input_matrix, cmap="gray",interpolation='none',origin='lower')
     #plt.show()
-    figure.savefig(audio_file[:-4]+"_sampling_window_length_"+str(sampling_window_length)+".png")
+    #figure.savefig(audio_file[:-4]+"_sampling_window_length_"+str(sampling_window_length)+".png")
+    figure.savefig(audio_file[:-4]+"_sampling_window_length_"+str(sampling_window_length)+"-_head_length_"+str(file_head_length)+".png")
     print("\tDone in %0.3fs." % (time() - t0))
 
 def main():
