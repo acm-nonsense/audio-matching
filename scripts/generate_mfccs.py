3e8808d4e8f0a2f84f17bd42a3fda859f1e9304f
Your Name <you@example.com>
2016-05-11 21:49:46 -0500

    Parallelized mfcc generation. Added progress percentage display for both scripts.

--- scripts/generate_mfccs.py
+++ scripts/generate_mfccs.py
@@ -10,6 +10,7 @@ import sys
 import os
 from time import time
 import warnings
+from multiprocessing import Pool,Value
 
 # actually, we're not computing the cross-dot=products, just the diagonals (sampling_window_length sums)... so maybe we can try doing that 
 # Should take an audio file, compute the mfcc (which is basically a feature vector), then computes the similarity matrix with square dimensions (dim(mfcc)/sampling_window_length)-squared by averaging over every cross=dot=product for a window of size sampling_window_length
@@ -17,12 +18,12 @@ import warnings
 
 
 sampling_window_length = 10
-file_head_length = 10 # in seconds
+file_head_length = 0 # in seconds
 warnings.filterwarnings("ignore", category=wav.WavFileWarning)
 
 def compute_mfcc(audio_file):
-    print("\tComputing MFCCs...")
-    t0 = time()
+    #print("\tComputing MFCCs...")
+    #t0 = time()
     (rate,sig) = wav.read(audio_file)
     if len(sig.shape) > 1:
         sig = np.mean(sig,axis=1) # Then we have stero channels and we need to flatten them
@@ -30,16 +31,16 @@ def compute_mfcc(audio_file):
         sig = sig[0:rate*file_head_length] # delete this after testing
     mfcc_feat = mfcc(sig,rate)
     #fbank_feat = logfbank(sig,rate) # potentially look at this later?
-    print("\tDone in %0.3fs." % (time() - t0))
+    #print("\tDone in %0.3fs." % (time() - t0))
     return mfcc_feat
 
 def standardize_mfcc_features(mfcc_features):
-    print("\tStandardizing MFCC Features...")
-    t0 = time()
+    #print("\tStandardizing MFCC Features...")
+    #t0 = time()
     feature_means = mean(mfcc_features)
     feature_stds = std(mfcc_features)
     standardized_features = (mfcc_features-feature_means)/feature_stds
-    print("\tDone in %0.3fs." % (time() - t0))
+    #print("\tDone in %0.3fs." % (time() - t0))
     return standardized_features
 
 '''
@@ -48,20 +49,41 @@ standardize_flag - True (default) to standardize the mfccs
 
 Outputs the generated mfcc .npz file to the output path 
 '''
-def generate_mfcc(input_file_path, output_file_path, standardize_flag):
+def generate_mfcc(item):
+    global index
+    global total_files
+
+    input_file_path = item["input_file_path"]
+    output_file_path = item["output_file_path"]
+    standardize_flag = item["flag"]
     mfcc = compute_mfcc(input_file_path)
     if standardize_flag:
         mfcc = standardize_mfcc_features(mfcc)
+    index.value += 1
     np.savez(output_file_path, mfcc)
-    
-    
+    print "\t{0:2.0f}%\b\b\b\b\b".format(100*index.value/total_files),
+    sys.stdout.flush()
+
+index = Value('i',0)
 
 def generate_all_mfccs(input_dir_path, output_dir_path,standardize_flag):
+    print("\tGenerating MFCCs...")
+    t0 = time()
+    global total_files
+    total_files = float(len( os.listdir(input_dir_path)))
+    pool = Pool(6)
+    items = []
     for target_file in os.listdir(input_dir_path):
         if target_file[-4:] == ".wav":
             input_file_path = os.path.join(input_dir_path,target_file)
             output_file_path = os.path.join(output_dir_path,"npz",target_file[:-4]+".mfcc.npz")
-            generate_mfcc(input_file_path,output_file_path,standardize_flag)
+            items.append({
+                "input_file_path": input_file_path,
+                "output_file_path": output_file_path,
+                "flag": standardize_flag
+            })
+    pool.map(generate_mfcc,items)
+    print("\tDone in %0.3fs." % (time() - t0))
 
 '''
 Inputs: source_path
@@ -86,6 +108,3 @@ if __name__ == "__main__":
         generate_all_mfccs(source_path,output_path,standardize_flag)
     else:
         generate_all_mfccs(source_path,output_path)
-
-
-        
--- scripts/generate_ssms.py
+++ scripts/generate_ssms.py
@@ -10,7 +10,7 @@ import os
 import sys
 from time import time
 import warnings
-from multiprocessing import Pool
+from multiprocessing import Pool,Value
 
 # actually, we're not computing the cross-dot=products, just the diagonals (sampling_window_length sums)... so maybe we can try doing that 
 # Should take an audio file, compute the mfcc (which is basically a feature vector), then computes the similarity matrix with square dimensions (dim(mfcc)/sampling_window_length)-squared by averaging over every cross=dot=product for a window of size sampling_window_length
@@ -22,10 +22,12 @@ sampling_window_length = 10
 file_head_length = 0 # in seconds
 
 warnings.filterwarnings("ignore", category=wav.WavFileWarning)
+gindex = Value('i',0)
+op_count = Value('d',0.0)
 index = 0
 def compute_similarity_matrix(mfcc_feat):
-    print("\tComputing similarity matrix...")
-    t0 = time()
+    #print("\tComputing similarity matrix...")
+    #t0 = time()
     sig_length = len(mfcc_feat)
     sig_item_length = len(mfcc_feat[0])
     mflat = mfcc_feat.reshape(sig_item_length*sig_length)
@@ -36,8 +38,8 @@ def compute_similarity_matrix(mfcc_feat):
         for j in range(batch_count):
             op_arr[i*batch_count+j,:sampling_window_length*sig_item_length] = mflat[i*sampling_window_length*sig_item_length:(i*sampling_window_length+sampling_window_length)*sig_item_length]
             op_arr[i*batch_count+j,sampling_window_length*sig_item_length:] = mflat[j*sampling_window_length*sig_item_length:(j*sampling_window_length+sampling_window_length)*sig_item_length]
-
-    #index = 0
+    global index
+    index = 0
     def ssm_item(batch):
         global index
         entry_l = batch[:len(batch)/2]
@@ -47,34 +49,32 @@ def compute_similarity_matrix(mfcc_feat):
         j = index%batch_count
         similarity_matrix[i:i+sampling_window_length,j:j+sampling_window_length] = entry/float(sampling_window_length)
         index += 1
-        #print "\t{0:2.0f}%\b\b\b\b\b".format(100*float(i*sampling_window_length*sig_length+j*sampling_window_length)/float(sig_length*sig_length)),
-        #sys.stdout.flush()
     map(ssm_item,op_arr)
-    print("\tDone in %0.3fs." % (time() - t0))
+    #print("\tDone in %0.3fs." % (time() - t0))
     return similarity_matrix
 
 def standardize_matrix(input_matrix):
-    print("\tStandardizing similarity matrix...")
-    t0 = time()
+    #print("\tStandardizing similarity matrix...")
+    #t0 = time()
     matrix_mean = mean(input_matrix)
     matrix_std = std(input_matrix)
     input_matrix = (input_matrix-matrix_mean)/matrix_std
-    print("\tDone in %0.3fs." % (jime() - t0))
+    #print("\tDone in %0.3fs." % (jime() - t0))
     return input_matrix
 
 def normalize_matrix(input_matrix):
-    print("\tNormalizing similarity matrix...")
-    t0 = time()
+    #print("\tNormalizing similarity matrix...")
+    #t0 = time()
     max = input_matrix.max()
     min = input_matrix.min()
     input_matrix -= min
     input_matrix /= (max-min)
-    print("\tDone in %0.3fs." % (time() - t0))
+    #print("\tDone in %0.3fs." % (time() - t0))
     return input_matrix
 
 def save_matrix(input_matrix,audio_file):
-    print("\tVisualizing similarity matrix...")
-    t0 = time()
+    #print("\tVisualizing similarity matrix...")
+    #t0 = time()
     # are we losing information if we try to cluster directly on the images? http://stackoverflow.com/questions/24185083/change-resolution-of-imshow-in-ipython
     #figure = plt.figure(figsize = (44100*sampling_window_length/1000,44100*sampling_window_length/1000))
     #plt.show()
@@ -86,7 +86,10 @@ def save_matrix(input_matrix,audio_file):
         plt.title(audio_file)
         plt.imshow(input_matrix, cmap="gray",interpolation='none',origin='lower')
         figure.savefig(filename+".png")
-    print("\tDone in %0.3fs." % (time() - t0))
+    #print("\tDone in %0.3fs." % (time() - t0))
+    gindex.value += 1
+    print "\t{0:2.0f}%\b\b\b\b\b".format(100*gindex.value/op_count.value),
+    sys.stdout.flush()
 
 def save_mfcc_similarity_matrix(path):
     with np.load(os.path.join(sys.argv[1],"npz",path)) as mfcc_feat_file:
@@ -96,17 +99,19 @@ def save_mfcc_similarity_matrix(path):
         save_matrix(normalized_matrix,path)
 
 def main():
+    global op_count
+    global output_images
     print "Starting to generate SSMs..."
     t0 = time()
-    global output_images
     if sys.argv[2] == "img":
         output_images = True
-    files = os.listdir(os.path.join(sys.argv[1],"npz")) 
+    files = os.listdir(os.path.join(sys.argv[1],"npz"))
     pool = Pool(6)
     items = []
     for item in files:
         if item[-8:-4] == "mfcc":
             items.append(item)
+    op_count.value = float(len(items))
     pool.map(save_mfcc_similarity_matrix,items)
     #map(save_mfcc_similarity_matrix,items)
     print("\tCompleted in %0.3fs." % (time() - t0))
